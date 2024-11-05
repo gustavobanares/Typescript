e314ef0c8dec7bfda67d5dcd6d96246c80054d0b
*Gustavo <gustavobbacco@gmail.com>
2024-11-05 20:33:31 -0300

    feat: variables in React

--- react_com_ts/src/App.tsx
+++ react_com_ts/src/App.tsx
@@ -1,11 +1,21 @@
 
 
 function App() {
+
+  // 1 - variaveis
+  const name: string = 'Gustavo'
+  const age: number = 20
+  const isWorking = true
   
   return (
     
       <div>
        <h1>Typescript com React</h1>
+       <h2>Nome: {name}</h2>
+       <p>Age: {age}</p>
+       {isWorking && (
+        <p>Está trabalhando!</p>
+       )}
       </div>
       
   )
